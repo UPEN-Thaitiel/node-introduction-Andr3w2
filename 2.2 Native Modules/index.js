910f5a3634af6c10dcfb2b64e6f41f8b4a24a9c2
Andr3w2 <jesusher615162@gmail.com>
2025-02-28 22:25:32 -0700

    avance

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,5 +1,15 @@
 // import fs module
-
+const fs = require(node.fs)
 //Using fs module create a file called message.txt and write a message in there
 
+fs.writeFile("message.txt", "hello world", (err) => {
+    if (err) throw err;
+    console.log("The file was created");
+})
+
 //Using fs module read the  file creatd and ans show the output in the terminal
+
+fs.readFile("message.txt", "uft8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -4,6 +4,10 @@
 
 // Use sillyname package to print a random silly name
 
+import generateName from "sillyname"
+var sillyname = generateName();
+console.log(sillyname);
+
 // Install superheroes package with npm install
 
 // Use superheroes package to print a random silly name
